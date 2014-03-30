50436f13047eaf2714b9dec9f602c2935aa72958
Tobias Foughman Lind <foughman.tobias@gmail.com>
2014-03-30 17:22:11 +0200

    Created the project in NetBeans

---
+++ ProjektRojarna/src/main/java/com/rojarna/projektrojarna/App.java
@@ -0,0 +1,13 @@
+package com.rojarna.projektrojarna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
