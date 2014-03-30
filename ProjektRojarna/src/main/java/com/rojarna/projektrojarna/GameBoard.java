e58aeac6ea426e0e442e6f38b77139d997174ab7
Joakim Milleson <joakim.milleson@hotmail.com>
2014-03-30 23:34:46 +0200

    Started some with the GameBoard class

--- ProjektRojarna/src/main/java/com/rojarna/projektrojarna/GameBoard.java
+++ ProjektRojarna/src/main/java/com/rojarna/projektrojarna/GameBoard.java
@@ -6,10 +6,43 @@
 
 package com.rojarna.projektrojarna;
 
+import java.util.List;
+
 /**
  *
  * @author Gaming
  */
 public class GameBoard {
     
+    private int mineCount;
+    private int width;
+    private int height;
+    
+    private boolean hasInit;
+    
+    private List[][] squares;
+    
+    public GameBoard(){
+        width = 7;
+        height = 7;
+        mineCount = 10;
+    }
+    
+    public GameBoard(int mineCount, int width, int height){
+        this.width = width;
+        this.height = height;
+        this.mineCount = mineCount;
+    }
+    
+    public void chooseSquare(int x, int y){
+        
+    }
+    
+    public void markSquare(int x, int y){
+        squares[x][y].markSquare();
+    }
+    
+    public State getState(int x, int y){
+        return squares[x][y].getState();
+    }
 }
