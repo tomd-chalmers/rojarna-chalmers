9e72a812326b2f1d5d10feacf751533c865f153b
Tobias Foughman Lind <foughman.tobias@gmail.com>
2014-03-30 20:17:03 +0200

    Started with the methods in the Square class

--- ProjektRojarna/src/main/java/com/rojarna/projektrojarna/Square.java
+++ ProjektRojarna/src/main/java/com/rojarna/projektrojarna/Square.java
@@ -8,8 +8,71 @@ package com.rojarna.projektrojarna;
 
 /**
  *
- * @author Gaming
+ * @author Tobias Foughman Lind
  */
 public class Square {
+    
+    private boolean isVisible;
+    
+    private Marking marking;
+    private State currentState;
+    
+    
+    private enum State{
+        EMPTY, ONE, TWO, THREE, FOUR, 
+        FIVE, SIX, SEVEN, EIGHT, MINE;
+    }
+    
+    private enum Marking{
+        NONE, FLAG, QUESTION;
+    }
+    
+    public Square(){
+        marking = Marking.NONE;
+        setState(State.EMPTY);
+    }
+    
+    public void setState(State state){
+        currentState = state;
+    }
+    
+    public void markSquare(){
+        if(!isVisible){
+            if(marking == Marking.NONE){
+                marking = Marking.FLAG;
+            }
+            else if(marking == Marking.FLAG){
+                marking = Marking.QUESTION;
+            }
+            else{
+                marking = Marking.NONE;
+            }
+        }
+    }
+    
+    public void increaseState(){
+        switch(currentState){
+            case ONE:
+                currentState = State.TWO;
+            case TWO:
+                currentState = State.THREE;
+            case THREE:
+                currentState = State.FOUR;
+            case FOUR:
+                currentState = State.FIVE;
+            case FIVE:
+                currentState = State.SIX;
+            case SIX:
+                currentState = State.SEVEN;
+            case SEVEN:
+                currentState = State.EIGHT;
+            case EIGHT:
+                currentState = State.ONE;
+        }
+    }
+    
+    public void setVisible(boolean value){
+        isVisible = value;
+    }
     
 }
