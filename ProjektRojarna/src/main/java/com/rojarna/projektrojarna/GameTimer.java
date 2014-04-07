bf2f20294de4e4942a153d8b9feacb0a5638c916
tomd-chalmers <tom.a.dahlen@gmail.com>
2014-04-08 01:09:52 +0200

    GameTimer redone and completed*
    *not realy.
    
    Redanoe the GameTimer and added a lot of mathod to be used.
    Still some small questions left...

--- ProjektRojarna/src/main/java/com/rojarna/projektrojarna/GameTimer.java
+++ ProjektRojarna/src/main/java/com/rojarna/projektrojarna/GameTimer.java
@@ -7,8 +7,6 @@ package com.rojarna.projektrojarna;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.Timer;
 
 
@@ -18,29 +16,94 @@ import javax.swing.Timer;
  * @revised Tom
  * @revised Tobias
  */
-public class GameTimer extends Timer{
-    private boolean descendingTime = false;
+public class GameTimer {
     
-    private int     timeLeft = 0;
+    private boolean isRunning=false;
+    private boolean dec = false;
+    private int time = 0;
+    private static int SEC = 1000;
     
-    // Timern från java.swing.Timer körs tydligen i samma tråd som alla andra
-    // swing objekt, så detta borde vara optimalt att använda tillsammans med
-    // GUI:n enligt de källor jag kollat.
+    private final Timer timer = new Timer(SEC, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(isRunning){
+                    if(dec){
+                        time--;
+                        // fire property change or something, for a view.
+                    }else{
+                        time++;
+                        // fire property change or something, for a view.
+                    }
+                }else{
+                    // do nothing...
+                }
+            }
+        });
     
-    // Sedan vet jag inte om vi borde extend:a eller endast ha en referens
-    // till en Timer i klassen så båda ligger här just nu.
-    private Timer gameTimer = null;
-    
-    
-    
-    //private List<ActionListener> timerListeners = new ArrayList<ActionListener>();
-    
-    
-    
-    public GameTimer(int delay, ActionListener listener){
-        super(delay, listener);
-        //gameTimer = new Timer(delay, listener);
         
-        //timerListeners.add(listener);
+    public GameTimer(){
+        isRunning = true;
+        timer.start();  //use method to start?
+    }
+    
+    public GameTimer(int time){
+        this.time=time;
+        isRunning = true;
+        dec = true;
+        timer.start(); //use method to start?
+    }
+    
+    
+    public int getTimeSec(){
+        return time;
+    }
+    
+    // giving the time in min
+    public String getTimeMin(){
+        int m = time/60;
+        int s = time-(60*m);
+        String min;
+        String sec;
+        
+        if(m < 10){
+            min = "0"+m;
+        }else{
+            min = ""+m;
+        }
+        if(s < 10){
+            sec = "0"+s;
+        }else{
+            sec = ""+s;
+        }
+        return min+":"+sec;
+    }
+    
+    public void pause(){
+        isRunning=false;
+    }
+    public void unPause(){
+        isRunning=true;
+    }
+    
+    public boolean afford(int a){
+        return time>a;
+    }
+    
+    public void removeTime(int a){
+        if(afford(a)){
+            time=time-a;
+        }
+        else{
+            //cant afford, do stuff?
+        }
+    }
+    
+    public void addTime(int a){
+        this.time=time+a;
+    }
+    
+    
+    public void Start(){  // needed? or start in construktor.
+        timer.start();
     }
 }
