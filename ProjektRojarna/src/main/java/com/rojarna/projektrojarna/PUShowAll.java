8781d0986d8f366029663d3e721c11d7595c2e40
tomd-chalmers <tom.a.dahlen@gmail.com>
2014-04-29 16:06:44 +0200

    PUShowAll compleasted, but non tested.

--- ProjektRojarna/src/main/java/com/rojarna/projektrojarna/PUShowAll.java
+++ ProjektRojarna/src/main/java/com/rojarna/projektrojarna/PUShowAll.java
@@ -6,19 +6,33 @@
 
 package com.rojarna.projektrojarna;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Tom
  */
 public class PUShowAll implements PowerupInterface{
     private final int COST=100;
+    private List<Boolean> listOfShown = new ArrayList<Boolean>();
     public void power(GameBoard gb, int x, int y) {
         for(int i = 0; i<gb.getHeight();i++){
             for(int j = 0; j<gb.getWidth();j++){
-                if(gb.getSquare(i,j).isVisible()){
-                    
+                listOfShown.add(gb.getSquare(i,j).isVisible());
+                if(!gb.getSquare(i,j).isVisible()){
+                    gb.getSquare(i, j).setVisible(true);
                 }
-                    //svårt... 
+            }
+        }
+        //wait a few sec here...
+        int k = 0;
+        for(int i = 0;i<gb.getHeight();i++){
+            for(int j = 0; j<gb.getWidth();j++){
+                if(!listOfShown.get(k)){
+                    gb.getSquare(i,j).setVisible(false);
+                }
+                k++;
             }
         }
     }
--- ProjektRojarna/src/main/java/com/rojarna/projektrojarna/PowerupInterface.java
+++ ProjektRojarna/src/main/java/com/rojarna/projektrojarna/PowerupInterface.java
@@ -11,5 +11,6 @@ package com.rojarna.projektrojarna;
  * @author Tom
  */
 public interface PowerupInterface {
-    
+    public void power(GameBoard gb, int x, int y);
+    public int getCost();
 }
